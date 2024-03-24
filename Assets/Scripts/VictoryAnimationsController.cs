5531ed3e1c6c224594c31e31d1efc6c9f45a303c
camilo1962 <108455047+camilo1962@users.noreply.github.com>
2024-03-24 20:44:17 +0100

    Initial commit

---
+++ Assets/Scripts/VictoryAnimationsController.cs
@@ -0,0 +1,25 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VictoryAnimationsController : MonoBehaviour
+{
+    [SerializeField] private GameObject victoryBox;
+    [SerializeField] private GameObject[] needToHide;
+
+    private void Start()
+    {
+        PlayVictorySound();
+        gameObject.transform.GetComponent<Image>().DOFade(0.6f, 2).OnComplete(() =>
+        {
+            foreach (var item in needToHide) item.SetActive(false);
+            victoryBox.SetActive(true);
+            victoryBox.transform.DOScale(1, 1);
+        });
+    }
+
+    private void PlayVictorySound()
+    {
+        if (PlayerPrefs.GetString("Sound") != "OFF") GetComponent<AudioSource>().Play();
+    }
+}
